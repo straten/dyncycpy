dc8c7ab7806b1e8ffb66aa9a00d9a657c86ac473
Willem van Straten <vanstraten.willem@gmail.com>
2024-11-12 11:50:59 +1300

    optionally deflect the DC bin

--- test/test_deflection.py
+++ test/test_deflection.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import pycyc
+
 import argparse
-import math
 import pickle
-import time
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.fft import fftshift
 
-import fista
-import pycyc
-from plotting import plot_intrinsic_vs_observed
 
 mpl.rcParams["image.aspect"] = "auto"
 
@@ -21,7 +18,7 @@ def analysis (base, gradient, delta):
 
     re_gradient = np.real(gradient)
     im_gradient = np.imag(gradient)
-    print(f"test_deflection: power in gradient={np.vdot(gradient, gradient)}")
+    print(f"test_deflection: power in gradient={np.real(np.vdot(gradient, gradient))}")
     print(f"test_deflection: power in Re[gradient]={np.vdot(re_gradient,re_gradient)}")
     print(f"test_deflection: power in Im[gradient]={np.vdot(im_gradient,im_gradient)}")
 
@@ -40,7 +37,7 @@ def analysis (base, gradient, delta):
     if delta is not None:
         re_delta = np.real(delta)
         im_delta = np.imag(delta)
-        print(f"test_deflection: power in delta={np.vdot(delta, delta)}")
+        print(f"test_deflection: power in delta={np.real(np.vdot(delta, delta))}")
         print(f"test_deflection: power in Re[delta]={np.vdot(re_delta,re_delta)}")
         print(f"test_deflection: power in Im[delta]={np.vdot(im_delta,im_delta)}")        
         plotthis = np.log10(np.abs(fftshift(delta)) + 1e-2)
@@ -61,24 +58,48 @@ def analysis (base, gradient, delta):
         print(f"test_deflection: gradient phase difference={np.angle(z)}")
         z /= np.abs(z)
 
+    pycyc.minimize_spectral_entropy(gradient)
+
+    plotthis = np.log10(np.abs(fftshift(gradient)) + 1e-2)
+    try:
+        fig, ax = plt.subplots(figsize=(8, 9))
+        img = ax.imshow(plotthis.T, aspect="auto", origin="lower", cmap="cubehelix_r")
+        fig.colorbar(img)
+        fig.savefig(base + "_gradient_mse.png")
+        plt.close()
+    except Exception:
+        print("##################################### gradient plot failed")
+    with open(base + "_gradient_mse.pkl", "wb") as fh:
+        pickle.dump(gradient, fh)    
+
 
 # do arg parsing here
-p = argparse.ArgumentParser()
-p.add_argument(
+parser = argparse.ArgumentParser()
+parser.add_argument(
     "--init",
     type=str,
     help="file containing the initial wavefield and intrinsic profile",
 )
-p.add_argument(
+parser.add_argument(
     "--amp",
     type=float,
     default=1e-6,
     help="relative amplitude of deflection",
 )
 
-args, files = p.parse_known_args()
+parser.add_argument('--dc', dest='dc', action='store_true', help="deflect the DC bin")
+parser.add_argument('--no-dc', dest='dc', action='store_false', help="do not deflect the DC bin")
+parser.set_defaults(dc=True)
+
+args, files = parser.parse_known_args()
 init = args.init
 relative_deflection = args.amp
+deflect_dc = args.dc
+
+if deflect_dc:
+    print("\ntest_deflection: deflecting DC")
+else:
+    print("\ntest_deflection: not deflecting DC")
 
 CS = pycyc.CyclicSolver()
 
@@ -139,7 +160,9 @@ for i in range(4):
 
     delta_factor = relative_deflection * np.exp(1.j * ph)
     delta = delta_factor * initial_doppler_delay
-    delta[0,0] = 0.0
+    
+    if not deflect_dc:
+        delta[0,0] = 0.0
 
     offset_doppler_delay = initial_doppler_delay + delta
 
