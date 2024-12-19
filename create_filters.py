1d21b38e12950c10c237050fcf684a4d5836e247
Willem van Straten <vanstraten.willem@gmail.com>
2024-12-20 10:40:38 +1100

    unload giant pulse filters in Dynamic Response extension of PSRFITS file

--- create_filters.py
+++ create_filters.py
@@ -4,13 +4,15 @@
 '''Generates average impulse responses that can be used as giant pulse detection filters,
    as described as "the first cycle" in Section 4 of Mahajan & van Kerkwijk (2023)'''
 
+import numpy as np
+import psrchive
+
 import argparse
 import time
 import pickle
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import numpy as np
 from scipy.fft import fftshift, fft, ifft
 from scipy.optimize import minimize
 from scipy.linalg import svd
@@ -30,42 +32,47 @@ produce_plots = False
 
 def add_filter_to_result () -> None:
 
-  global result
-  global result_index
+    global result
+    global result_index
 
-  if store is None:
-      error("no stored data")
+    if store is None:
+        error("no stored data")
 
-  print(f"performing SVD on {store_index} spectra")
+    print(f"performing SVD on {store_index} spectra")
 
-  subset = store[:,:store_index,:]
-  nsamp = store.shape[2]
+    subset = store[:,:store_index,:]
+    nsamp = store.shape[2]
 
-  subset = np.reshape(subset, (2*store_index,nsamp))
-  if result is None:
-      result = np.zeros((1, nsamp), dtype=np.complex128)
-  else:
-      result.resize((result_index+1, nsamp))
+    subset = np.reshape(subset, (2*store_index,nsamp))
+    if result is None:
+        result = np.zeros((1, nsamp), dtype=np.complex128)
+    else:
+        result.resize((result_index+1, nsamp))
 
-  U, s, Vh = svd (subset)
-  print(f"singular values {s}")
-  result[result_index,:] = Vh[0,:]
+    U, s, Vh = svd (subset)
+    print(f"singular values {s}")
+    result[result_index,:] = Vh[0,:]
 
-  if produce_plots:
-      fig, axs = plt.subplots(1,2)
-      toplot = np.copy(np.real(result[result_index,:]))
-      axs[0].plot(toplot)
-      axs[0].set(ylabel="Re[v(t)]", xlabel="Time (sample index)")
-      toplot = np.copy(np.imag(result[result_index,:]))
-      axs[1].plot(toplot)
-      axs[1].set(ylabel="Im[v(t)]", xlabel="Time (sample index)")
+    if produce_plots:
+        fig, axs = plt.subplots(1,2)
+        toplot = np.copy(np.real(result[result_index,:]))
+        axs[0].plot(toplot)
+        axs[0].set(ylabel="Re[v(t)]", xlabel="Time (sample index)")
+        toplot = np.copy(np.imag(result[result_index,:]))
+        axs[1].plot(toplot)
+        axs[1].set(ylabel="Im[v(t)]", xlabel="Time (sample index)")
 
+    result_index += 1
+
+
+first_datestr = None
 first_second = 0
 current_time = 0
 file_index = 0
 
 def get_current_offset (filename) -> float:
 
+    global first_datestr
     global first_second
     global current_time 
     global file_index 
@@ -78,6 +85,7 @@ def get_current_offset (filename) -> float:
 
     if file_index == 0:
       first_second = the_second
+      first_datestr = datestr
 
     remainder = filename[dot+1:]
     dot = remainder.find('.')
@@ -94,68 +102,74 @@ def get_current_offset (filename) -> float:
     return current_offset
 
 
-def create_filters (files, interval) -> None:
+def create_filters (files, interval, template) -> None:
 
-  global result_index
-  global store 
-  global store_index
-  global file_index
-  global current_time
+    global store 
+    global store_index
+    global file_index
+    global current_time
 
-  for file in files:
+    for file in files:
 
-    current_offset = get_current_offset(file)
+        current_offset = get_current_offset(file)
 
-    if current_offset > interval:
-        add_filter_to_result ()
-        current_time += interval
-        store_index = 0
-        result_index += 1
+        if current_offset > interval:
+            add_filter_to_result ()
+            current_time += interval
+            store_index = 0
 
-    f = open(file, "rb")
-    f.seek(4096) # skip the 4k DADA header
+        f = open(file, "rb")
+        f.seek(4096) # skip the 4k DADA header
 
-    data = np.fromfile(f, dtype=np.complex64)
-    # print(f"{data.size} complex<float> loaded")
+        data = np.fromfile(f, dtype=np.complex64)
+        # print(f"{data.size} complex<float> loaded")
 
-    data = np.reshape (data, (2, data.size//2), order='F')
-    # print(f"new shape={data.shape}")
+        data = np.reshape (data, (2, data.size//2), order='F')
+        # print(f"new shape={data.shape}")
 
-    if store is None:
-        store = np.zeros((2, len(files), data.size//2), dtype=np.complex128)
+        if store is None:
+            store = np.zeros((2, len(files), data.size//2), dtype=np.complex128)
 
-    store[:,store_index,:] = data
-    file_index += 1
-    store_index += 1
+        store[:,store_index,:] = data
+        file_index += 1
+        store_index += 1
 
-    if produce_plots and file_index == 1:
-        fig, axs = plt.subplots(1,2)
+        if produce_plots and file_index == 1:
+            fig, axs = plt.subplots(1,2)
 
-        for ipol in range(2):
-            toplot = np.real(data[ipol])
-            axs[ipol].plot(toplot)
-            axs[ipol].set(ylabel="Re[v(t)]", xlabel="Time (sample index)")
-        
-        filename = "first_pulse.png"
-        plt.savefig(filename)
-        plt.close()
+            for ipol in range(2):
+                toplot = np.real(data[ipol])
+                axs[ipol].plot(toplot)
+                axs[ipol].set(ylabel="Re[v(t)]", xlabel="Time (sample index)")
+            
+            filename = "first_pulse.png"
+            plt.savefig(filename)
+            plt.close()
 
-  if store_index > 0:
-    print(f"adding last interval to result current_offset/interval={current_offset}/{interval}")
-    add_filter_to_result()
- 
+    if store_index > 0:
+        print(f"adding last interval to result current_offset/interval={current_offset}/{interval}")
+        add_filter_to_result()
+    
 
 def main() -> None:
-    """Plot the cyclic spectrum in four different ways."""
-    import argparse
+
+    """Create average giant pulses that can be used as filters."""
+
+    global result
 
     # do arg parsing here
     p = argparse.ArgumentParser()
     p.add_argument(
-        "--interval",
+        "-interval",
         type=float,
         default=20,
         help="the integration interval in seconds",
+    )
+    p.add_argument(
+        "-template",
+        type=str,
+        required=True,
+        help="the archive file used as a template",
     )
 
     args, files = p.parse_known_args()
@@ -163,8 +177,37 @@ def main() -> None:
     vargs = vars(args)
     create_filters(files, **vargs)
 
+    print(f"{result.shape=}")
+
     with open("giant_pulse_filters.pkl", "wb") as fh:
         pickle.dump(result, fh)
+
+    arch = psrchive.Archive_load(args.template)
+    arch.resize(0)
+
+    ntime = result.shape[0]
+    nchan = result.shape[1]
+    result = np.reshape(result,ntime*nchan)
+
+    ext = arch.add_dynamic_response()
+    ext.set_nchan(nchan)
+    ext.set_ntime(ntime)
+    ext.set_npol(1)
+    ext.resize_data()
+    ext.set_data(result)
+
+    start_time = psrchive.MJD(first_datestr)
+    end_time = start_time + args.interval * ntime
+    ext.set_minimum_epoch (start_time)
+    ext.set_maximum_epoch (start_time)
+
+    cfreq = arch.get_centre_frequency()
+    bw = arch.get_bandwidth()
+
+    ext.set_minimum_frequency (cfreq - 0.5*bw)
+    ext.set_maximum_frequency (cfreq + 0.5*bw)
+
+    arch.unload("giant_pulse_filters.fits")
 
 if __name__ == "__main__":
   main()
